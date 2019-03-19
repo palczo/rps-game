a3a2801b39341435a24cd9fef06f12133ba96e75
Sylwia Palczowska <sylwiapalczowska@gmail.com>
2019-03-19 21:15:09 +0100

    Add some changes

--- js/script.js
+++ js/script.js
@@ -1,32 +1,24 @@
-var argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber;
+var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
 
 /**
- * Get Move Name
+ * Describe this function...
  */
 function getMoveName(argMoveId) {
   console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
   if (argMoveId == 1) {
     return 'kamień';
+  } else if (argMoveId == 2) {
+    return 'papier';
+  } else if (argMoveId == 3) {
+    return 'nożyce';
   } else {
     printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
     return 'kamień';
   }
-  if (argMoveId == 2) {
-    return 'papier';
-  } else {
-    printMessage('Nie znam ruchu o id ' + argMoveId + 'Zakładam, że chodził o "papier".');
-    return 'papier';
-  }
-  if (argMoveId == 3) {
-    return 'nożyce';
-  } else {
-    printMessage('Nie znam ruchu o id ' + argMoveId + 'Zakładam, że chodzi i "nożyce".');
-    return 'nożyce';
-  }
 }
 
 /**
- * Display Result
+ * Describe this function...
  */
 function displayResult(argPlayerMove, argComputerMove) {
   console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
@@ -35,15 +27,7 @@ function displayResult(argPlayerMove, argComputerMove) {
   } else {
     printMessage('Przegrywasz :(');
   }
-
-  // printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-  // if (argPlayerMove == 'papier' && argComputerMove == 'nożyce') {
-  //  printMessage('Wygrywasz!');
-  // } else {
-  //  printMessage('Przegrywasz :(');
-  //}
-  //printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-  
+  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
 playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('wybór ruchu gracza to: ' + playerInput);
